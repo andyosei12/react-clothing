fb2305fa37410cbed8a6704fafdb23921bb142ec
Drew Osei <andrewowusu21@yahoo.com>
2021-11-23 02:03:50 +0000

    routing menu items (#3)
    Co-authored-by: Andy-Osei <andrew.owusu@novanest.net>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 // info: Components Import
 import Homepage from "./pages/homepage/Homepage";
 
+const Hatspage = (props) => {
+  console.log(props);
+  return <h1>Hats page</h1>;
+};
+
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/hats" component={Hatspage} />
+    </Switch>
   );
 }
 
