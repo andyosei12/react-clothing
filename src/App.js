a11e956cc4243d2b8d92857edcb159fb26c5db5c
Andy-Osei <andrew.owusu@novanest.net>
2021-11-25 05:50:20 +0000

    added shop page and collection preview

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-// info: Components Import
+// info: Components and Pages Import
 import Homepage from "./pages/homepage/Homepage";
-
-const Hatspage = (props) => {
-  console.log(props);
-  return <h1>Hats page</h1>;
-};
+import Shop from "./pages/shop/Shop";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
-      <Route path="/hats" component={Hatspage} />
+      <Route path="/shop" component={Shop} />
     </Switch>
   );
 }
