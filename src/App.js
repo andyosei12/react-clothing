2eae9f29d6bb12845b3a762f0e5a8a4c1f95a1cd
Drew Osei <andrewowusu21@yahoo.com>
2021-12-07 06:02:36 +0000

    google sign in with firebase (#7)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { auth } from "./firebase/firebase.utils";
 import "./App.css";
 
 // info: Components and Pages Import
@@ -8,17 +9,40 @@ import Shop from "./pages/shop/Shop";
 import Signin from "./pages/signin/Signin.page";
 import Header from "./components/header/Header";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={Shop} />
-        <Route path="/signin" component={Signin} />
-      </Switch>
-    </>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/signin" component={Signin} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
