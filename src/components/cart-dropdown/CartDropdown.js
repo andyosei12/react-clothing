4c076b944128917643d9d0fc1b12bd255a4e5cf3
Andy-Osei <andrew.owusu@novanest.net>
2021-12-30 11:32:31 +0000

    feat-displaying cart items

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import CartItem from "../cart-items/CartItems";
 import CustomButton from "../custom-button/CustomButton";
 
 import "./CartDropdown.scss";
 
-function CartDropdown() {
+function CartDropdown({ cartItems }) {
+  console.log(cartItems);
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} items={item} />
+        ))}
+      </div>
       <CustomButton>Go to checkout</CustomButton>
     </div>
   );
 }
 
-export default CartDropdown;
+const mapStateToProps = ({ cart }) => ({
+  cartItems: cart.cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
