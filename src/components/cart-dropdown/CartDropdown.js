5f58a79267b67f8421c7809988d4b4231ed76d8b
Andy-Osei <andrew.owusu@novanest.net>
2022-01-11 09:35:05 +0000

    feat-added the checkout items page

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,26 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import { selectCartItems } from "../../redux/cart/cart-selectors";
+import { toggleCartDropDown } from "../../redux/cart/cart-actions";
 import CartItem from "../cart-items/CartItems";
 import CustomButton from "../custom-button/CustomButton";
 
 import "./CartDropdown.scss";
 
-function CartDropdown({ cartItems }) {
+function CartDropdown({ cartItems, dispatch }) {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} items={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} items={item} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
-      <CustomButton>Go to checkout</CustomButton>
+      <Link to="/checkout">
+        <CustomButton onClick={() => dispatch(toggleCartDropDown())}>
+          Go to checkout
+        </CustomButton>
+      </Link>
     </div>
   );
 }
 
-const mapStateToProps = (state) => ({
-  cartItems: selectCartItems(state),
-});
+const mapStateToProps = () =>
+  createStructuredSelector({
+    cartItems: selectCartItems,
+  });
 
 export default connect(mapStateToProps)(CartDropdown);
