b398471ae587932a7e2906257f276ce948e5092c
Drew Osei <andrewowusu21@yahoo.com>
2022-01-12 13:57:03 +0000

    feat-persisting cart items (#18)
    Co-authored-by: Andy-Osei <andrew.owusu@novanest.net>

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
+import { persistStore } from "redux-persist";
+// import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+// const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+// export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export const persistor = persistStore(store);
