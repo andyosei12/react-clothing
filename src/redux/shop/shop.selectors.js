a3558567feb9caa3d6bacbced3efc12650356559
Andy-Osei <andrew.owusu@novanest.net>
2022-01-27 06:07:09 +0000

    feat-routing singular shop item

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,18 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
 
 export const selectShop = (state) => state.shop;
 
 export const selectShopData = createSelector(
   [selectShop],
   (shop) => shop.shopData
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShopData],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector([selectShopData], (shopData) => shopData[collectionUrlParam])
 );
