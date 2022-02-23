6a05809566ffe7cd129f84f9eadc159380b5d2ce
Drew Osei <andrewowusu21@yahoo.com>
2022-02-23 06:18:39 +0000

    feat/fetching shop data from firebase (#25)
    Co-authored-by: Andy-Osei <andrew.owusu@novanest.net>

--- src/components/collection-preview/CollectionPreview.js
+++ src/components/collection-preview/CollectionPreview.js
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./CollectionPreview.module.scss";
 import CollectionItem from "../collection-item/CollectionItem";
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, path }) => {
   return (
     <div className={styles["collection-preview"]}>
-      <h1 className={styles.title}>{title.toUpperCase()}</h1>
+      <Link to={`${path.url}/${title.toLowerCase()}`}>
+        <h1 className={styles.title}>{title.toUpperCase()}</h1>
+      </Link>
       <div className={styles.preview}>
         {items
           .filter((item, idx) => idx < 4)
